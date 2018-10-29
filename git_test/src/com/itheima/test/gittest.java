8975c4ed03354233853c7cda545ab3303cb07825
shixihua <948051221@qq.com>
2018-10-29 08:47:39 +0800

    第一次提交gittest

---
+++ git_test/src/com/itheima/test/gittest.java
@@ -0,0 +1,15 @@
+package com.itheima.test;
+
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("刺激");
+        System.out.println("刺激");
+        System.out.println("刺激");
+        System.out.println("年轻");
+
+
+        int i=10*10;
+
+    }
+
+}
